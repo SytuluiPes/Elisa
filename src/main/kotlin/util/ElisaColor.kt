66dc34f9bcfbc41c863b486d7df3bf072b40dfeb
artem <artemkovalev2014@gmail.com>
2024-05-15 09:34:41 +0300

    renamed to Elisa

--- src/main/kotlin/util/ElisaColor.kt
+++ src/main/kotlin/util/ElisaColor.kt
@@ -3,9 +3,9 @@ package util
 import androidx.compose.ui.graphics.Color
 
 object ElisaColor {
-    val Black = Color(0x001a1a1d)
-    val Gray = Color(0x004e4e50)
-    val DarkRed = Color(0x6f2232)
-    val Raspberry = Color(0x00950740)
-    val Red = Color(0x00c3073f)
+    val Black = Color(0xff1a1a1d)
+    val Gray = Color(0xff4e4e50)
+    val DarkRed = Color(0xff6f2232)
+    val Raspberry = Color(0xff950740)
+    val Red = Color(0xffc3073f)
 }
